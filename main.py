583d48d72074ed5eb5f1100d59cf6713b8e6d85f
dmitryprogrammer <dpischalka@gmail.com>
2026-09-25 00:14:06 +0300

    first logic for open user files

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-message = "Hello World!"
-print(message)
+import sys
 
+pathBase = "./data/"
+filename = pathBase + sys.argv[1]
+sought = sys.argv[2:]
+
+with open(filename, "r", encoding="utf-8") as file:
+    for line in file:
+        print("line - ", line.strip())

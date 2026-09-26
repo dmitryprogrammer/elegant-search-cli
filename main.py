be8e1f34ba85375e9d1947a74f56a16db3db9cdf
dmitryprogrammer <dpischalka@gmail.com>
2026-09-26 12:59:41 +0300

    Created simple write/read for file

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ pathBase = "./data/"
 filename = pathBase + sys.argv[1]
 sought = sys.argv[2:]
 
-with open(filename, "r", encoding="utf-8") as file:
+with open(filename, "w+", encoding="utf-8") as file:
+    for line in range(5):
+        file.write(f"line${str(line)} \n")
+
+    file.seek(0)
+
     for line in file:
         print("line - ", line.strip())
